c4778bde5c3ea22b98d4dcd1776f54b128f3e6ba
Graeme Stirling <graemejstirling@gmail.com>
2021-12-15 15:14:28 +0000

    refactor

--- PeakArrayIndex/peakArray.js
+++ PeakArrayIndex/peakArray.js
@@ -1,28 +1,16 @@
-function assertEquals(array) { 
-  if (array.length < 3){
-    return -1;
-  }
-  else {
-    return findIndex(array);
-  }
-}
-
-// condition, when sum of array[0,i] == array[i+1, array.length] return i 
-
-
-function findIndex(array) {
+function peak(array) {
   for (let i = 1; i < array.length-1; i++) {
     const lhs = array.slice(0, i).reduce(add, 0);
     const rhs = array.slice(i+1).reduce(add, 0);
     if (rhs == lhs){
       return i;
     }
-  } 
+  }
+  return -1;
 }
 
 function add(accumulator, a) {
   return accumulator + a;
 }
 
-
-module.exports = assertEquals
+module.exports = peak
