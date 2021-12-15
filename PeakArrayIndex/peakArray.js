51d1261a08b70fb6ceb15aa0dddd9dc83a00836e
Graeme Stirling <graemejstirling@gmail.com>
2021-12-15 15:10:46 +0000

    Green third test

--- PeakArrayIndex/peakArray.js
+++ PeakArrayIndex/peakArray.js
@@ -7,10 +7,22 @@ function assertEquals(array) {
   }
 }
 
+// condition, when sum of array[0,i] == array[i+1, array.length] return i 
+
+
 function findIndex(array) {
-    if (array.first == array.last){
-      return 1;
+  for (let i = 1; i < array.length-1; i++) {
+    const lhs = array.slice(0, i).reduce(add, 0);
+    const rhs = array.slice(i+1).reduce(add, 0);
+    if (rhs == lhs){
+      return i;
     }
+  } 
 }
+
+function add(accumulator, a) {
+  return accumulator + a;
+}
+
 
 module.exports = assertEquals
