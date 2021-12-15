901c4069f6b5c05b208f056db249ac1ff420017c
Graeme Stirling <graemejstirling@gmail.com>
2021-12-15 14:29:35 +0000

    Green first test

--- PeakArrayIndex/peakArray.js
+++ PeakArrayIndex/peakArray.js
@@ -1,5 +1,9 @@
-function assertEquals() { 
-  return "yes";
+function assertEquals(array) { 
+  if (array.length < 3){
+    return -1;
+  }
+  else {
+  }
 }
 
 module.exports = assertEquals
--- PeakArrayIndex/peakArray.test.js
+++ PeakArrayIndex/peakArray.test.js
@@ -1,6 +1,9 @@
 const assertEquals = require('./peakArray') 
 
-test('setup test', () => {
-  const output = assertEquals();
-  expect(output).toEqual("yes"); 
+// assume we are given one or two elements, we should recieve -1 for those cases
+
+test('returns -1 if given array with 1 - 2 indexes', () => {
+  expect(assertEquals([1,1])).toEqual(-1)
 })
+
+
