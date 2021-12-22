4287f2ca74210fce17e8d46bc30e75c2b8e3bc31
Graeme Stirling <graemejstirling@gmail.com>
2021-12-22 15:41:25 +0000

    Second test

--- Javascript/ArrayFilter/model.test.js
+++ Javascript/ArrayFilter/model.test.js
@@ -1,6 +1,10 @@
 const { expect } = require('@jest/globals')
 const arrayFilter = require('./model')
 
-test('', () => {
+test('ignores odd numbers', () => {
   expect(arrayFilter([1,3])).toEqual([])
+})
+
+test('returns only even numbers', () => {
+  expect(arrayFilter([1,2,3,4])).toEqual([2,4])
 })
