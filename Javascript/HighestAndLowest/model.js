0bb3a1271c6c7179f3439e13bfd117153f68b93a
Graeme Stirling <graemejstirling@gmail.com>
2021-12-22 15:16:59 +0000

    Passing second test

--- Javascript/HighestAndLowest/model.js
+++ Javascript/HighestAndLowest/model.js
@@ -1,5 +1,7 @@
 function highAndLow(numbers){
-  return numbers
+  const num = numbers.split(" ")
+  const lastNum = num.length  
+  return `${num[0]} ${num[lastNum - 1]}`
 }
 
 module.exports = highAndLow
