3f540ff42456e2834d0f4baebafdfb446776115c
Graeme Stirling <graemejstirling@gmail.com>
2021-12-22 15:13:16 +0000

    Failing second test

--- Javascript/HighestAndLowest/model.test.js
+++ Javascript/HighestAndLowest/model.test.js
@@ -1,6 +1,10 @@
 const { expect } = require('@jest/globals')
 const highAndLow = require('./model')
 
-test('returns number', () => {
+test('returns two numbers', () => {
   expect(highAndLow("1 2")).toEqual("1 2")
+})
+
+test('Picks highest and lowest from three numbers', () => {
+  expect(highAndLow("1 2 3")).toEqual("1 3")
 })
