60871856172889a783ba3a8aff182ac6a40b27ea
Graeme Stirling <graemejstirling@gmail.com>
2021-12-22 15:25:45 +0000

    final refactor

--- Javascript/HighestAndLowest/model.js
+++ Javascript/HighestAndLowest/model.js
@@ -1,5 +1,5 @@
 function highAndLow(numbers){
-  const num = numbers.split(" ").sort() 
+  const num = numbers.split(" ").sort(function(a, b){return a-b}) 
   return `${num[0]} ${num[num.length - 1]}`
 }
 
--- Javascript/HighestAndLowest/model.test.js
+++ Javascript/HighestAndLowest/model.test.js
@@ -12,3 +12,7 @@ test('Picks highest and lowest from three numbers', () => {
 test('Can find lowest and highest when numbers are not in order', () => {
   expect(highAndLow("1 3 2")).toEqual("1 3")
 })
+
+test('Can find lowest and highest when numbers are not in order', () => {
+  expect(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4")).toEqual("-9 42")
+})
