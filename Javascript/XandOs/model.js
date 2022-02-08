a0347326c99bcd7e650eda93adc7b8862abaaceb
Graeme Stirling <graemejstirling@gmail.com>
2022-02-08 15:33:59 +0000

    Solution

--- Javascript/XandOs/model.js
+++ Javascript/XandOs/model.js
@@ -1,6 +1,21 @@
 function XO(string){
-  return true;
+  let x = 0 
+  let o = 0 
+  string.split("").length;
+  for (var i = 0; i < string.split("").length; i++) {
+    if (findCharacter(string, i) == "o") {
+      o += 1;
+    }
+    else if (findCharacter(string, i) == "x") {
+      x += 1;
+    }
+  }
+  const result = (x == o) ? true : false
+  return result;
 }
 
+function findCharacter(string, i) {
+  return string.split("")[i].toLowerCase()
+}
 
 module.exports = XO
--- Javascript/XandOs/model.test.js
+++ Javascript/XandOs/model.test.js
@@ -2,6 +2,22 @@ const { expect } = require('@jest/globals')
 const XO = require('./model')
 
 test('returns true when given even number x and os', () => {
-  expect(XO("xo")).toEqual(true)
+  expect(XO("ooxx")).toEqual(true)
 })
+
+test('returns true in a different order', () => {
+  expect(XO("xoxo")).toEqual(true)
+})
+
+test('returns false if there is uneven order', () => {
+  expect(XO("xoxoxoxOxoxoxoxoo")).toEqual(false)
+})
+
+test('returns true if no x and o', () => {
+  expect(XO("fthfthfth")).toEqual(true)
+})
+
+
+
+
 
